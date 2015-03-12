91033f6d1ab341a39babcb1f8a0dffe68cbe53a3
Wendell Smith <wackywendell@gmail.com>
2015-03-12 11:23:09 -0400

    Documentation update, trying to get .travis.yml to do its job

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 /*!
 
-A basic library for finding primes, providing a basic Iterator over all primes.
-It is not as fast as `slow_primes`, but it is meant to be easy to use!
+A basic library for finding primes, providing a basic Iterator over all primes. It is not as fast as
+`slow_primes`, but it is meant to be easy to use!
 
 The simplest usage is simply to create an `Iterator`:
 
@@ -15,17 +15,9 @@ for (ix, n) in pset.iter().enumerate().take(10) {
 }
 ```
 
-This library provides methods for generating primes, testing whether a number
-is prime, and factorizing numbers. Most methods generate primes lazily, so
-only enough primes will be generated for the given test, and primes are cached
-for later use.
-
-To use, see `PrimeSet`, a class which handles the Sieve and has multiple
-methods for iterating over primes.
-
-This also provides a few functions unconnected to `PrimeSet`, which will be
-faster for the first case, but slower in the long term as they do not use any
-caching of primes.
+This library provides methods for generating primes, testing whether a number is prime, and
+factorizing numbers. Most methods generate primes lazily, so only enough primes will be generated
+for the given test, and primes are cached for later use.
 
 [*Source*](https://github.com/wackywendell/primes)
 
@@ -60,6 +52,12 @@ println!("The first prime after 1000 is the {}th prime: {}", ix, n);
 
 assert_eq!(pset.find(n), (ix, n));
 ```
+
+For more info on use, see `PrimeSet`, a class which handles the Sieve and has multiple methods for
+iterating over primes.
+
+This also provides a few functions unconnected to `PrimeSet`, which will be faster for the first
+case, but slower in the long term as they do not use any caching of primes.
 
 */
 #![feature(core)]
@@ -115,7 +113,7 @@ impl PrimeSet {
     pub fn new() -> PrimeSet {
         PrimeSet{lst:vec!(2,3)}
     }
-    
+
     /// Finds one more prime, and adds it to the list
     pub fn expand(&mut self) {
         let mut l : u64 = self.lst[self.lst.len()-1] + 2;
@@ -128,48 +126,48 @@ impl PrimeSet {
                     break;
                 }
             };
-            
+
             if remainder != 0 {
                 self.lst.push(l);
                 break;
             };
-            
+
             l += 2;
             sql = sqrt_floor(l);
         }
     }
-    
+
     /// Number of primes found so far
     pub fn len(&self) -> usize {
         self.lst.len()
     }
-    
+
     /// Return all primes found so far as a slice
     pub fn list<'a>(&'a self) -> &'a [u64] {
         &self.lst[..]
     }
-    
+
     /// Iterator over all primes not yet found
     pub fn generator<'a>(&'a mut self) -> PrimeSetIter<'a> {
         let myn = self.len();
         PrimeSetIter{p:self, n:myn, expand:true}
     }
-    
+
     /// Iterator over all primes, starting with 2. If you don't care about the "state" of the
     /// PrimeSet, this is what you want!
     pub fn iter<'a>(&'a mut self) -> PrimeSetIter<'a> {
         PrimeSetIter{p:self, n:0, expand:true}
     }
-    
+
     //~ pub fn iter_once(&'self mut self) -> PrimeSetIter<'self> {
         //~ PrimeSetIter{p:self, n:0, expand:false}
     //~ }
-    
+
     /// Iterator over just the primes found so far
     pub fn iter_vec<'a>(&'a self) -> slice::Iter<'a, u64> {
         self.lst.iter()
     }
-    
+
     /// Find the next largest prime from a number
     /// Returns (idx, prime)
     /// Note that if n is prime, then the output will be (idx, n)
@@ -179,7 +177,7 @@ impl PrimeSet {
         }
         self.find_vec(n).unwrap()
     }
-    
+
     /// Check if a number is prime
     /// Note that this only requires primes up to n.sqrt() to be generated, and will generate
     /// them as necessary on its own.
@@ -192,13 +190,13 @@ impl PrimeSet {
         }
         panic!("This iterator should not be empty.");
     }
-    
+
     /// Find the next largest prime from a number, if it is within the already-found list
     /// Returns (idx, prime)
     /// Note that if n is prime, then the output will be (idx, n)
     pub fn find_vec(&self, n: u64) -> Option<(usize, u64)> {
         if n > *(self.lst.last().unwrap_or(&0)){ return None;}
-        
+
         let mut base : usize = 0;
         let mut lim : usize = self.len();
 
@@ -217,7 +215,7 @@ impl PrimeSet {
         }
         return Some((base, self.lst[base]));
     }
-    
+
     /// Get the nth prime, even if we haven't yet found it
     pub fn get(&mut self, index : &usize) -> &u64 {
 		for _ in (0..(*index as isize) + 1 - (self.lst.len() as isize)){
@@ -225,7 +223,7 @@ impl PrimeSet {
 		}
         self.lst.index(index)
 	}
-	
+
 	/// Get the prime factors of a number, starting from 2, including repeats
 	pub fn prime_factors(&mut self, n: u64) -> Vec<u64> {
 		if n == 1 {return Vec::new();}
@@ -239,7 +237,7 @@ impl PrimeSet {
 				if curn == 1 {return lst;}
 				m = sqrt_ceil(curn);
 			}
-			
+
 			if p > m {
 				lst.push(p);
 				return lst;
@@ -266,9 +264,9 @@ impl<'a> Iterator for PrimeSetIter<'a> {
             }
         }
         self.n += 1;
-        
+
         let m = self.p.lst[self.n-1];
-        
+
         Some(m)
     }
 }
@@ -354,9 +352,9 @@ fn test_find(){
 #[test]
 fn test_primes(){
     let mut pset = PrimeSet::new();
-    
+
     // note: some are repeated, because the pset list grows as it goes
-    
+
     assert!(!pset.is_prime(1));
     assert!(!is_prime(1));
     assert!(pset.is_prime(2));
@@ -379,7 +377,7 @@ fn test_primes(){
 #[test]
 fn test_factors(){
 	let mut pset = PrimeSet::new();
-	
+
 	let ns = [  (1, vec!()),
                 (2, vec!(2)),
                 (3, vec!(3)),
@@ -394,7 +392,7 @@ fn test_factors(){
                 (100, vec!(2,2,5,5)),
                 (121, vec!(11, 11)),
                 ];
-    
+
     // Test unique factors
     for &(n, ref v) in ns.iter(){
         assert_eq!(pset.prime_factors(n), *v);
@@ -408,10 +406,10 @@ fn test_factors(){
 
         assert_eq!(unique_factors, unique_factors_exp);
     }
-	
+
 	pset = PrimeSet::new();
 	assert_eq!(pset.prime_factors(12), vec!(2,2,3));
-	
+
 }
 
 
