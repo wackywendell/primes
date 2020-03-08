556b244c6e0001f8742af0c0583d8688c3aa266e
Wendell Smith <wackywendell@gmail.com>
2020-03-08 13:27:32 -0400

    Added benchmarks for trialdivision and sieve

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,10 +1,8 @@
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 
-use primes::{PrimeSet, TrialDivision};
+use primes::{PrimeSet, Sieve, TrialDivision};
 
 fn bench_primes(c: &mut Criterion) {
-    let mut group = c.benchmark_group("TrialDivision::find");
-
     let mut sizes: Vec<u64> = Vec::new();
     for &base in &[5_000, 50_000] {
         for size in 2..=20 {
@@ -14,12 +12,25 @@ fn bench_primes(c: &mut Criterion) {
     sizes.sort();
     sizes.dedup();
 
+    let mut group = c.benchmark_group("TrialDivision::find");
     for &size in sizes.iter() {
         group.throughput(Throughput::Elements(size));
         group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
             b.iter(|| {
-                let mut td = TrialDivision::new();
-                black_box(td.find(size))
+                let mut pset = TrialDivision::new();
+                black_box(pset.find(size))
+            })
+        });
+    }
+    group.finish();
+
+    let mut group = c.benchmark_group("Sieve::find");
+    for &size in sizes.iter() {
+        group.throughput(Throughput::Elements(size));
+        group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
+            b.iter(|| {
+                let mut pset = Sieve::new();
+                black_box(pset.find(size))
             })
         });
     }
